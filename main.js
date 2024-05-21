e4eb31024c4bacdf765b2f6d13dd4e5321e1cb65
OgPupa <125026631+OgPupa@users.noreply.github.com>
2024-05-21 10:47:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+
+const rates = {};
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementCNY = document.querySelector('[data-value="CNY"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const elementGBP = document.querySelector('[data-value="GBP"]');
+const elementJPY = document.querySelector('[data-value="JPY"]');
+
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
+const select = document.querySelector('#select');
+
+const dailyOperations = {};
+
+const convertButton = document.querySelector('#convert-button')
+
+const currentDate = new Date();
+const currentDay = `${currentDate.getDate()}-${(currentDate.getMonth() + 1)}-${currentDate.getFullYear()}`;
+
+getCurrencies();
+
+setInterval(getCurrencies, 5000);
+
+async function getCurrencies () {
+    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    const data = await response.json();
+    const result = await data;
+  
+
+    rates.USD = result.Valute.USD;
+    rates.CNY = result.Valute.CNY;
+    rates.EUR = result.Valute.EUR;
+    rates.GBP = result.Valute.GBP;
+    rates.JPY = result.Valute.JPY;
+
+    console.log(rates);
+
+    elementUSD.textContent = rates.USD.Value.toFixed(2);
+    elementCNY.textContent = rates.CNY.Value.toFixed(2);
+    elementEUR.textContent = rates.EUR.Value.toFixed(2);
+    elementGBP.textContent = rates.GBP.Value.toFixed(2);
+    elementJPY.textContent = rates.JPY.Value.toFixed(2);
+
+    //цветокор в зависимости от курса
+    if (rates.USD.Value > rates.USD.Previous) {
+        elementUSD.classList.add('top');
+    } else {
+        elementUSD.classList.add('bottom');
+    }
+
+    if (rates.CNY.Value > rates.CNY.Previous) {
+        elementCNY.classList.add('top');
+    } else {
+        elementCNY.classList.add('bottom');
+    }
+
+    if (rates.EUR.Value > rates.EUR.Previous) {
+        elementEUR.classList.add('top');
+    } else {
+        elementEUR.classList.add('bottom');
+    }
+
+    if (rates.GBP.Value > rates.GBP.Previous) {
+        elementGBP.classList.add('top');
+    } else {
+        elementGBP.classList.add('bottom');
+    }
+
+    if (rates.JPY.Value > rates.JPY.Previous) {
+        elementJPY.classList.add('top');
+    } else {
+        elementJPY.classList.add('bottom');
+    }
+
+}
+
+input.oninput = convertValue;
+select.oninput = convertValue;
+
+function convertValue(){
+    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
+
+}
+
+convertButton.addEventListener('click', function () {
+    if (!dailyOperations[select.value]) {
+        dailyOperations[select.value] = {};
+    }
+
+    if (!dailyOperations[select.value][currentDay]) {
+        dailyOperations[select.value][currentDay] = 0;
+    }
+
+    if (dailyOperations[select.value][currentDay] + parseFloat(input.value) > 1000) {
+        alert(`Для ${select.value} операция не может превышать 1000 единиц в течение одного дня.`);
+        return;
+    }
+
+    dailyOperations[select.value][currentDay] += parseFloat(input.value);
+
+
+    alert('Конвертация проведена успешно.');
+});
