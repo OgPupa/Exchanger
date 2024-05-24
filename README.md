03750041aacb53a69ad541415dd0dc781425cd70
Ilya Yushkin <i@kenchikk.ru>
2024-05-24 14:52:30 +0300

    Добавлена генерация квитанций, дамп базы данных

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Exchanger
 course work 
+DataBase
+Файл dump-course-202405241445 содержит дамп базы данных PostgreSQL. Необходимо воссоздат бд course и таблицу convert. Данные для подключения
+postgres://postgres:Googleapple123@localhost:5432/course
--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jung-kurt/gofpdf"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +23,32 @@ func index(w http.ResponseWriter, r *http.Request) {
 	err = t.ExecuteTemplate(w, "index", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func generatePDF(w http.ResponseWriter, id int, convTime time.Time, amountIn, amountOut float64, currencyIn, currencyOut string) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Receipt")
+	pdf.Ln(20)
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.Cell(40, 10, fmt.Sprintf("Operation number: %d", id))
+	pdf.Ln(10)
+	pdf.Cell(40, 10, fmt.Sprintf("Conversion time: %s", convTime.Format("2006-01-02 15:04:05")))
+	pdf.Ln(10)
+	pdf.Cell(40, 10, fmt.Sprintf("Entry Amount: %.2f", amountIn))
+	pdf.Ln(10)
+	pdf.Cell(40, 10, fmt.Sprintf("Exit Amount: %.2f", amountOut))
+	pdf.Ln(10)
+	pdf.Cell(40, 10, fmt.Sprintf("Input Currency: %s", currencyIn))
+	pdf.Ln(10)
+	pdf.Cell(40, 10, fmt.Sprintf("Outgoing Currency: %s", currencyOut))
+
+	err := pdf.Output(w)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Ошибка генерации PDF: %v", err), http.StatusInternalServerError)
 	}
 }
 
@@ -56,23 +83,27 @@ func save(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close(context.Background())
 
-	currentTime := time.Now() // получение текущего времени
+	currentTime := time.Now()
 
-	_, err = conn.Exec(
+	var id int
+	err = conn.QueryRow(
 		context.Background(),
-		"INSERT INTO Convert (amount_in, amount_out, conv_time, currency_in, currency_out) VALUES ($1, $2, $3, $4, $5)",
+		"INSERT INTO Convert (amount_in, amount_out, conv_time, currency_in, currency_out) VALUES ($1, $2, $3, $4, $5) RETURNING id",
 		inputCourse,
 		outputCourse,
 		currentTime,
 		take,
 		give,
-	)
+	).Scan(&id)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Ошибка выполнения запроса: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Header().Set("Content-Disposition", "inline; filename=\"receipt.pdf\"")
+
+	generatePDF(w, id, currentTime, inputCourse, outputCourse, take, give)
 }
 
 func handleFunc() {
